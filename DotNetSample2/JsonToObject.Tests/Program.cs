ee4524a24ad1c1bd7956748bb02097d19e8918a4
doyoulaikeme <673615750@qq.com>
2020-06-10 11:58:20 +0800

    Add files via upload

---
+++ DotNetSample2/JsonToObject.Tests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonToObject.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "{ \"name\":\"runoob\", \"alexa\":10000, \"site\":null }";
+
+            string classCode = JsonClassGenerator.GenerateString(json, CodeLanguage.CSharp);
+
+            Console.WriteLine(classCode);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
